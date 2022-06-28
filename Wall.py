425fde10c15c33b437b9b67e9941db63e106d9b7
Rohan Patankar <rohanpatankar926@gmail.com>
2022-06-28 16:04:58 +0530

    Initial Commit

--- Wall.py
+++ Wall.py
@@ -2,6 +2,6 @@ class Wall:
     def __init__(self):
         pass
     
-    def wall_update(Strength, wall_attacked, wall):
+    def wall_update(self,Strength, wall_attacked, wall):
         wall[wall_attacked] = Strength
         return wall
