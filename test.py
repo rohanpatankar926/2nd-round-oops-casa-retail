673e3471672881a25e0ebeed2e4198b46e51c3a3
Rohan Patankar <rohanpatankar926@gmail.com>
2022-06-28 20:21:55 +0530

    Initial Commit

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
 from Vallah import Vallah
 
-wall = {'N':0, 'S':0, 'E':0, 'W':0}
+class Test:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        self.wall = {'N':0, 'S':0, 'E':0, 'W':0}
 
-total_days = int(input("Enter total number of days of nomadic attacks --> "))
-answer = 0
-for i in range(total_days):
-    attack_per_day = int(input("Per day attacks --> "))
+        self.total_days = int(input("Enter total number of days of nomadic attacks --> "))
+        self.answer = 0
+        for i in range(self.total_days):
+            self.attack_per_day = int(input("Per day attacks --> "))
 
-    for j in range(attack_per_day):
-        wall_attacked = input("Enter wall attacked (N,S,E,W) --> ")
-        strength = int(input("Stregth of weapon X --> "))
-        answer = answer + Vallah().attack(strength, wall_attacked, wall)
-print(answer)
+            for j in range(self.attack_per_day):
+                self.wall_attacked = input("Enter wall attacked (N,S,E,W) --> ")
+                self.strength = int(input("Stregth of weapon X --> "))
+                self.answer = self.answer + Vallah().attack(self.strength, self.wall_attacked, self.wall)
+        print (self.answer)
 
+if __name__=="__main__":
+    Test().main()        
